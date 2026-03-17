1117904c014cd2e02d4d6921df029be76853da8a
tommybrunelle29 <tommy.brunelle29@gmail.com>
2026-03-17 16:00:01 -0400

    update

--- dictionnaire.py
+++ dictionnaire.py
@@ -1,11 +1,19 @@
+#Liste de dictionnaires
 infos = [
     {"marque": "Toyota", "modele": "Corolla"},
-    {"marque": "Nissan", "modele": "Altima"}
+    {"marque": "Nissan", "modele": "Altima"},
+    {"marque": "Toyota", "modele": "Tacoma"}
 ]
 
+#Réponse donnée par l'utilisateur
 question_01 = input("Quelle marque: ")
 
-for voiture in infos:
-    if question_01 == voiture["marque"]:
-        print(voiture)
-        break
+#Pour chaque dictionnaire dans la liste "infos"
+for dictionnaire in infos:
+
+    #Si la réponse de l'utilisateur est présente dans un dictionnaire dans la liste "infos"
+    if question_01 == dictionnaire["marque"]:
+
+        #Imprime le/les dictionnaire.s où la réponse est présente.
+        print(dictionnaire)
+        
