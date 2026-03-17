3b33470d97dc0cbd35866d7d6238117c0ebd3555
Vinithepoo0901 <vincentgoulet0901@gmail.com>
2026-03-17 15:33:04 -0400

    b

---
+++ csv.py
@@ -0,0 +1,15 @@
+infos = [
+    
+    {"Marque":"Toyota", "Modele":"Corolla"},
+    {"Marque":"Nissan", "Modele":"Altima"}
+
+]
+
+question_01 = input("Quel Marque?")
+
+if question_01 in infos :
+    print(infos["question_01"])
+
+else :
+    print("erreur")
+    
