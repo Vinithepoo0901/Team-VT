82f4be04f55f34b248209a4d1f365e816674e4f9
Vinithepoo0901 <vincentgoulet0901@gmail.com>
2026-03-24 15:23:35 -0400

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,5 +1,8 @@
-print("Guide de selection de vehicule pour concessionnaire")
-print("Quel est la marque du vehicule?")
-print("Quel est votre budget ?")
-print("Quel est votre preference de sorte")
-print("Quel consommation votre vehicule devrait avoir?")
+import tkinter as tk 
+
+root = tk.Tk()
+root.title("")
+
+button_1 = tk.Button(root, text= "button_1").grid(row=1, sticky=tk.W)
+
+root.mainloop()
