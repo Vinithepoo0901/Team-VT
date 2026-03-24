ee758a3b18983c92e6b1fed530fbb9b98b2f96bb
Vinithepoo0901 <vincentgoulet0901@gmail.com>
2026-03-24 15:24:35 -0400

    update

--- csv.py
+++ csv.py
@@ -1,5 +1,17 @@
-print("Guide de selection de vehicule pour concessionnaire")
-print("Quel est la marque du vehicule?")
-print("Quel est votre budget ?")
-print("Quel est votre preference de sorte")
-print("Quel consommation votre vehicule devrait avoir?")
+import tkinter as tk
+
+
+def on_button_click():
+    print("ppp")
+
+root = tk.Tk()
+root.title("Button Example")
+root.geometry("300x200") 
+
+my_button = tk.Button(root, text="Click Me", command=on_button_click).grid(row=0, sticky=tk.W)
+my_button_2 = tk.Button(root, text="Click moi", command=on_button_click).grid(row=1, sticky=tk.W)
+
+
+
+
+root.mainloop()
