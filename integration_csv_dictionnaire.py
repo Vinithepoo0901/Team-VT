9edd5c497462d15a33c75a4eec5b6b5eeb6e224d
Vinithepoo0901 <vincentgoulet0901@gmail.com>
2026-03-31 15:23:50 -0400

    wf

--- integration_csv_dictionnaire.py
+++ integration_csv_dictionnaire.py
@@ -1,4 +1,4 @@
-csv = open("test.csv", "r") #r pour lecture
+csv = open("Projet-2-excel.csv", "r") #r pour lecture
 liste_voiture = csv.readlines() #lire tout et vient creer une liste et chaque ligne de la liste est un element
 
 infos = [] #Liste vide
@@ -15,8 +15,9 @@ for ligne in liste_voiture:
     #transforme les elements de la liste de chaque ligne en dictionnaire
     dictionnaire = {
         "Marque": detail[0],
-        "Modèle": detail[1],
-        "Transmission": detail[2]
+        "Type": detail[1],
+        "Consomation" : detail[2],
+        "Prix de depart" : detail[3]
     }
 
     #on ajoute le set dans la liste infos
@@ -25,23 +26,5 @@ for ligne in liste_voiture:
 #ferme le fichier
 csv.close()
 
-#Réponse donnée par l'utilisateur
-question_01 = input("Quelle marque: ")
-question_02 = input("Quelle transmission: ")
+print (detail[0])
 
-#Variable pour empecher que le message d<erreur print plusieurs fois dans le for loop.
-option_existe = False
-
-#Pour chaque set dans la liste "infos"
-for dictionnaire in infos:
-
-    # Si la réponse de l'utilisateur est présente dans un des Set de la liste "infos"
-    if question_01 == dictionnaire["Marque"] and question_02 == dictionnaire["Transmission"]:
-
-        option_existe = True
-
-        # Imprime le/les Set.s où la réponse est présente
-        print(dictionnaire)
-
-if option_existe == False: 
-    print("Nous n'avons aucune voiture qui correspond à l'entièreté de vos critères. Désolé.")
