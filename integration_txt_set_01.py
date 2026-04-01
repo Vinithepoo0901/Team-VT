0e4acde2cf900e860d12918793b29993d8609def
tommybrunelle29 <tommy.brunelle29@gmail.com>
2026-04-01 16:05:22 -0400

    update, set trop complexe, switch a tuple

--- integration_txt_set_01.py
+++ integration_txt_set_01.py
@@ -1,7 +1,7 @@
 txt = open("Toyota_Corolla.txt", "r") #r pour lecture
 liste_voiture = txt.readlines() #lire tout et vient creer une liste et chaque ligne de la liste est un element
 
-infos = [] #Liste vide
+infos_01 = [] #Liste vide
 
 #Pour chaque ligne dans le .txt
 for ligne in liste_voiture:
@@ -12,34 +12,12 @@ for ligne in liste_voiture:
     #split pour creer des elements
     detail = ligne.split(", ")
 
-    #transforme les elements de la liste de chaque ligne en set
-    voiture_set = {
-        detail[0], detail[1], detail[2], detail[3]
-    }
+    #transforme les elements de la liste de chaque ligne en tuple
+    voiture_set = (detail[0], detail[1]) 
 
-    #on ajoute le set dans la liste infos
-    infos.append(voiture_set)
+    #on ajoute le tuple dans la liste infos
+    infos_01.append(voiture_set)
 
 #ferme le fichier
 txt.close()
 
-#Réponse donnée par l'utilisateur
-question_01 = input("Quelle marque: ")
-question_02 = input("Quelle transmission: ")
-
-#Variable pour empecher que le message d<erreur print plusieurs fois dans le for loop.
-option_existe = False
-
-#Pour chaque set dans la liste "infos"
-for voiture_set in infos:
-
-    # Si la réponse de l'utilisateur est présente dans un des Set de la liste "infos"
-    if question_01 in voiture_set and question_02 in voiture_set:
-
-        option_existe = True
-
-        # Imprime le/les Set.s où la réponse est présente
-        print(voiture_set)
-
-if option_existe == False: 
-    print("Nous n'avons aucune voiture qui correspond à l'entièreté de vos critères. Désolé.")
